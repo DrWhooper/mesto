0a631a40fe2845de8db18671ad8e622157bfb1d7
Anton Kazaryan <kazarian.anton.wp@mail.ru>
2023-01-22 16:54:22 +0300

    version_1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let editButton = document.querySelector('.profile__edit-button');
 let popupOpen = document.querySelector('.popup');
+let popupBtn = document.querySelector('.popup__btn')
 
 let closeBtn = document.querySelector('.popup__close');
 
@@ -11,16 +12,23 @@ closeBtn.addEventListener('click', function popupClose() {
     popupOpen.classList.add('popup_opened');
 })
 
+popupBtn.addEventListener('click', function popupClose() {
+  popupOpen.classList.add('popup_opened');
+})
+
 
 let formElement = document.querySelector('.popup__form')
 let nameInput = formElement.querySelector('.popup__input-name')
 let jobInput = formElement.querySelector('.popup__input-jobs')
 let profileName = document.querySelector('.profile__name')
+let profileJobs = document.querySelector('.profile__jobs')
 
+console.log(nameInput.value)
 function handleFormSubmit (evt) {
     evt.preventDefault();
-    profileName = nameInput.getAttribute('value');
-    jobInput.getAttribute('value');
+    profileName.textContent = nameInput.value;
+    profileJobs.textContent = jobInput.value;
+
 }
 
-formElement.addEventListener("submit", formElement);
+formElement.addEventListener("submit", handleFormSubmit);
