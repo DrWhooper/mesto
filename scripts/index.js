81a24a7b40e3659dd7e3b7ec87e9feb68cae2549
Anton Kazaryan <kazarian.anton.wp@mail.ru>
2023-01-24 18:40:02 +0300

    refact

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+let editButton = document.querySelector('.profile__edit-button');
+let popupOpen = document.querySelector('.popup');
+let popupBtn = document.querySelector('.popup__btn');
+
+let closeBtn = document.querySelector('.popup__close');
+
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__form_input_name');
+let jobInput = formElement.querySelector('.popup__form_input_jobs');
+let profileName = document.querySelector('.profile__name');
+let profileJobs = document.querySelector('.profile__jobs');
+
+
+function popupShow() {
+  popupOpen.classList.add('popup_opened');
+}
+
+function popupClose() {
+  popupOpen.classList.remove('popup_opened');
+}
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJobs.textContent = jobInput.value;
+  popupClose()
+}
+
+editButton.addEventListener('click', popupShow)
+closeBtn.addEventListener('click', popupClose);
+popupBtn.addEventListener('click', popupClose)
+formElement.addEventListener("submit", handleFormSubmit);
